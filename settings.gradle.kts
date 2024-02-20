61672faa9d448ccdad406d5b7dac73e2453a4ae2
Dirk Grappendorf <dg@steva.digital>
2024-02-20 10:46:57 +0100

    build: add moko resources

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,16 +15,16 @@ pluginManagement {
         val kotlinVersion = extra["kotlin.version"] as String
         val agpVersion = extra["agp.version"] as String
         val composeVersion = extra["compose.version"] as String
+        val mokoResourcesVersion = extra["moko.resources.version"] as String
 
         kotlin("jvm").version(kotlinVersion)
         kotlin("multiplatform").version(kotlinVersion)
         kotlin("plugin.serialization").version(kotlinVersion)
         kotlin("android").version(kotlinVersion)
-
         id("com.android.application").version(agpVersion)
         id("com.android.library").version(agpVersion)
-
         id("org.jetbrains.compose").version(composeVersion)
+        id("dev.icerock.mobile.multiplatform-resources").version(mokoResourcesVersion)
     }
 }
 
